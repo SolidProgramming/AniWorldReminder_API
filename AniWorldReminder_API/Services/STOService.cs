1874712dc5060757290d3889a81b2a42ade469e0
Luca <luca.weidmann@gmx.de>
2026-08-10 17:29:14 +0200

    nuget updates s.to -> serienstream.cx

--- AniWorldReminder_API/Services/STOService.cs
+++ AniWorldReminder_API/Services/STOService.cs
@@ -15,7 +15,7 @@ namespace AniWorldReminder_API.Services
             ILogger<STOService> logger,
             Interfaces.IHttpClientFactory httpClientFactory,
             ITMDBService tmdbService)
-            : base(logger, httpClientFactory, "http://186.2.175.5/", "S.TO", StreamingPortal.STO, tmdbService)
+            : base(logger, httpClientFactory, "https://serienstream.cx", "S.TO", StreamingPortal.STO, tmdbService)
         {
         }
 
